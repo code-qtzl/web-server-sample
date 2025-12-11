7031e2cb2471d286dd3bd8d994efa7add1cc61fe
code-qtzl <azepeda90@outlook.com>
2025-12-11 16:07:39 -0600

    [WIP] including dotenv and refactored the architecture of the code to match the example

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
 import express from 'express';
-import { Request, Response } from 'express';
+import 'dotenv/config';
+
+import { handlerReadiness } from './api/readiness.js';
 
 const app = express();
-const PORT = 8080;
-
-// app.use(express.static('.'));
+const PORT = process.env.PORT;
 
 app.use('/app', express.static('./src/app'));
 
-app.get('/healthz', (request, response) => {
-	return response
-		.set('Content-Type', 'text/plain; charset=utf-8')
-		.status(200)
-		.send('OK');
-});
+app.get('/healthz', handlerReadiness);
 
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
